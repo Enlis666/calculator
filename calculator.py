96f12ae07a54fca63bd604f3434145622db6c271
Andrey TUryshev <enlis666@gmail.com>
2024-07-05 20:15:03 +0700

    reset

--- calculator.py
+++ calculator.py
@@ -1,19 +1,23 @@
 class Calculator:
-
+    """
+    Данный класс состоит из статичных методов
+    Методы принимают два числа и возвращают результат выражение
+    """
     @staticmethod
-    def add(num1: float, num2: float) -> float:
+    def add(num1: int, num2: int) -> int:
         return num1 + num2
 
     @staticmethod
-    def sub(num1: float, num2: float) -> float:
+    def sub(num1: int, num2: int) -> int:
         return num1 - num2
 
     @staticmethod
-    def mul(num1: float, num2: float) -> float:
+    def mul(num1: int, num2: int) -> int:
         return num1 * num2
 
     @staticmethod
-    def div(num1: float, num2: float) -> float:
+    def div(num1: int, num2: int) -> int:
         if num2 == 0:
             raise ValueError('На ноль делить нельзя!')
-        return num1 / num2
+
+        return int(num1 / num2)
--- main.py
+++ main.py
@@ -4,19 +4,26 @@ from calculator import Calculator
 def working_with_calculator(expression: str) -> float:
     """
     Функция производит расчет запроса пользователя.
-    Принимает строку выражения в формате 'число оператор число'
+    Принимает строку выражения в формате 'число : оператор : число'
     Возвращает результат выражение в виде числа
     """
-    result_expression = expression.split()
 
+    result_expression = expression.split()  # Разбиваем строку в список
+
+    # Проверяем данные по указанным в ТЗ требованиям
     if len(result_expression) != 3:
         raise ValueError('Формат данных некорректный!')
 
     try:
-        num1, operator, num2 = float(result_expression[0]), result_expression[1], float(result_expression[2])
+        num1, operator, num2 = int(result_expression[0]), result_expression[1], int(result_expression[2])
     except ValueError:
         raise ValueError('Формат данных некорректный!')
 
+    if not (1 <= num1 <= 10) or not (1 <= num2 <= 10):
+        raise ValueError('Целые числа должны быть введены в дипапазоне от 0 до 10!')
+
+    # Выбираем подходящий метод и возвращаем результат работы в виде числа
+    # Если оператор не подходит под требования - выдаем ошибку
     cal = Calculator()
 
     if operator == '+':
@@ -30,12 +37,23 @@ def working_with_calculator(expression: str) -> float:
     raise ValueError('Введенный оператор явялется некорректным! \nДопускаются операторы: +, -, /, *')
 
 
-if __name__ == '__main__':
-    print('Добро пожаловать в калькулятор 1.0')
+def main() -> None:
+    """
+    Основаная функция
+    Принимает запрос пользователя и выдает ответ
+    """
+    try:
+        user = input('\nВведите данные для расчета в формате (number_one operator number_two): ')
+        print(f'Результат: {working_with_calculator(user)}')
 
+    except ValueError as error:
+        print(f'Ошибка: {error}')
+
+
+if __name__ == '__main__':
+    print('Добро пожаловать в калькулятор 1.0 ^_^')
+    # В тз указанно, что при срабатывание ошибки нужно завершать программу
+    # Для более удобного тестирование зациклил функцию
+    # Прошу меня не ругать :)
     while True:
-        try:
-            user = input('\nВведите данные для расчета в формате (number operator number): ')
-            print(f'Ответ: {working_with_calculator(user)}')
-        except ValueError as error:
-            print(f'Ошибка: {error}')
+        main()
